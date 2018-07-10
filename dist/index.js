2abcef0458e7e4ef3b8778a717c19ed622c73663
Mikhail <mixa63rus@mail.ru>
2018-07-10 17:38:14 +0400

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _readlineSync = require('readline-sync');
+
+var _readlineSync2 = _interopRequireDefault(_readlineSync);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var actual = _readlineSync2.default.question('Your answer: ');
+
+exports.default = actual;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJhY3R1YWwiLCJyZWFkbGluZVN5bmMiLCJxdWVzdGlvbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7Ozs7OztBQUVBLElBQU1BLFNBQVNDLHVCQUFhQyxRQUFiLENBQXNCLGVBQXRCLENBQWY7O2tCQUVlRixNIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHJlYWRsaW5lU3luYyBmcm9tICdyZWFkbGluZS1zeW5jJztcclxuXHJcbmNvbnN0IGFjdHVhbCA9IHJlYWRsaW5lU3luYy5xdWVzdGlvbignWW91ciBhbnN3ZXI6ICcpO1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgYWN0dWFsOyJdfQ==
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+const actual = readlineSync.question('Your answer: ');
+
+export default actual;
