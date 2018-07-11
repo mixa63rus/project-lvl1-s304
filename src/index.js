d3172ed5bbdb659821036e3ab113bc5b829d5592
mixa63rus <37752948+mixa63rus@users.noreply.github.com>
2018-07-11 17:28:41 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import readlineSync from 'readline-sync';
 
-export default function(){
-const personName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + personName + '!');
-}
+export default () => {
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".\n');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!\n`);
+const gameRound = 3;
+for (let i = 0; i < gameRound; i += 1) {
+  const number = Math.floor(Math.random() * 100);
+  console.log('Question: ' + number);
+  const answer = readlineSync.question('Your answer: ');
+  const normalAnswer = number % 2 === 0 ? 'yes' : 'no';
+  if (answer !== normalAnswer) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${normalAnswer}'.`);
+    console.log(`Let's try again, ${name}!`);
+    return;
+  }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${name}!`);
+};
