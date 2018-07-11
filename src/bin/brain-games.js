f0eabe42233046a5f2ff9971b197d313b4d1bbe4
mixa63rus <37752948+mixa63rus@users.noreply.github.com>
2018-07-11 13:37:59 +0400

    Update brain-games.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
-<<<<<<< HEAD
 import question from '..';
 
 console.log('Welcome to the Brain Games!');
 question();
-=======
-
-console.log('Welcome to the Brain Games!');
->>>>>>> 1e34277c93bde34ae6395abbf836a10c9867c1bb
