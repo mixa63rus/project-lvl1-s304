c9e3815f34c5d9e9a757d2ef53a4e0ade4577f78
Mikhail <mixa63rus@mail.ru>
2018-07-13 10:54:46 +0400

    fix-1

--- src/games/even.js
+++ src/games/even.js
@@ -4,7 +4,7 @@ import getRandomNumber from '../utils';
 
 const descriptionGame = 'Answer "yes" if number even otherwise answer "no".\n';
 const isEven = number => number % 2 === 0;
-const correctAnswer = (number) => (isEven(number) ? 'yes' : 'no');
+const correctAnswer = number => (isEven(number) ? 'yes' : 'no');
 
 const getPair = () => {
   const number = getRandomNumber(100);
